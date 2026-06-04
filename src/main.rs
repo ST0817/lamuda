16ccf1ed1707becf66655bea1c9425bb6866163d
ST0817 <136716138+ST0817@users.noreply.github.com>
2026-06-04 20:38:49 +0900

    add simple REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::ExitCode;
+
+use rustyline::{DefaultEditor, error::ReadlineError};
+
+const HISTORY_FILE_NAME: &str = ".lamuda_history";
+
+fn main() -> ExitCode {
+    let mut editor = DefaultEditor::new().unwrap();
+
+    if editor.load_history(HISTORY_FILE_NAME).is_err() {
+        println!("no previous history")
+    }
+
+    loop {
+        match editor.readline("❯ ") {
+            Ok(input) if input.is_empty() => {}
+            Ok(input) => {
+                editor.add_history_entry(&input).unwrap();
+                println!("input: {input}")
+            }
+            Err(ReadlineError::Eof) => {
+                editor.append_history(HISTORY_FILE_NAME).unwrap();
+                break ExitCode::SUCCESS;
+            }
+            Err(error) => {
+                eprintln!("Error: {error}");
+                editor.append_history(HISTORY_FILE_NAME).unwrap();
+                break ExitCode::FAILURE;
+            }
+        }
+    }
 }
